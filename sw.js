4eb74a866ba77c35b65afc0cb0ae7c662b8c400c
wolfieLouis <miguewolf06@gmail.com>
2026-03-10 05:54:20 -0400

    Update asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 const CACHE='rincon-fe-v2';
-const ASSETS=['/','/index.html','/manifest.json'];
+const ASSETS=[
+  '/PrayerandLoveApp/',
+  '/PrayerandLoveApp/index.html',
+  '/PrayerandLoveApp/manifest.json'
+];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
